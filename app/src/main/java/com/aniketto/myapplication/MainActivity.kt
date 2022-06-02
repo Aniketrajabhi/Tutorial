be8b87c30c222972b050cbace7229f15692a5d59
Aniketrajabhi <aniketrajabhi123@gmail.com>
2022-06-03 01:21:30 +0530

    Initial Tutorial application files
    *  Synced till 15th video of phillip lackner from Android Fundamentals for beginners playlist.

---
+++ app/src/main/java/com/aniketto/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.aniketto.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etAge = findViewById<EditText>(R.id.etAge)
+        val etCountry = findViewById<EditText>(R.id.etCountry)
+        val btnApply = findViewById<Button>(R.id.btnApply)
+
+        btnApply.setOnClickListener {
+            val name = etName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val country = etCountry.text.toString()
+            val person = Person(name,age,country)
+            Intent( this, SecondActivity::class.java ).also {
+                it.putExtra("EXTRA_PERSON",person)
+                startActivity(it)
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/aniketto/myapplication/Person.kt
@@ -0,0 +1,10 @@
+package com.aniketto.myapplication
+
+import java.io.Serializable
+
+data class Person(
+    val name :String,
+    val age: Int,
+    val country : String
+) :Serializable
+
