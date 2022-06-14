13c162220e279abf96b204dcc3b13c4310443f0a
Aniketrajabhi <aniketrajabhi123@gmail.com>
2022-06-15 03:59:13 +0530

    ...

--- app/src/main/java/com/aniketto/myapplication/MainActivity.kt
+++ app/src/main/java/com/aniketto/myapplication/MainActivity.kt
@@ -1,32 +1,41 @@
 package com.aniketto.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(), NewsItemClicked {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val flFragment = findViewById<FrameLayout>(R.id.flFragment)
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.rv1)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val items = fetchData()
-        val adapter: NewsListAdapter= NewsListAdapter(items ,  this)
-        recyclerView.adapter = adapter
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment , firstFragment)
+            commit()
         }
-
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this , "clicked item is $item" , Toast.LENGTH_SHORT).show()
+        btn1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment , firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btn2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment , secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
-}
-
-private fun fetchData() : ArrayList<String>{
-    val list = ArrayList<String>()
-    for (i in 0 until 21){
-        list.add("Items $i")
-    }
-    return list
 }
