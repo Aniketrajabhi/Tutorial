219821291d6fafc62a29f6cf79fb12c676ec3549
Aniketrajabhi <aniketrajabhi123@gmail.com>
2022-06-15 02:41:17 +0530

    added click button on Recycler view for viewing item

--- app/src/main/java/com/aniketto/myapplication/MainActivity.kt
+++ app/src/main/java/com/aniketto/myapplication/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.aniketto.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewsItemClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.rv1)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val items = fetchData()
+        val adapter: NewsListAdapter= NewsListAdapter(items ,  this)
+        recyclerView.adapter = adapter
         }
+
+    override fun onItemClicked(item: String) {
+        Toast.makeText(this , "clicked item is $item" , Toast.LENGTH_SHORT).show()
     }
+}
+
+private fun fetchData() : ArrayList<String>{
+    val list = ArrayList<String>()
+    for (i in 0 until 21){
+        list.add("Items $i")
+    }
+    return list
+}
